713904ad964c1e3fb8918e3013012a8ea8590a0b
checorazza <ceciicorazza@gmail.com>
2025-01-31 12:37:55 -0300

    add tutorial section. add string formatting tutorial

---
+++ Tutorial/String Formatting/string_format.py
@@ -0,0 +1,9 @@
+# Variables
+name = "John"
+age = 25
+
+# Using f-string
+print(f"Hello, {name}. You are {age} years old.")
+
+# Using format method
+print("Hello, {}. You are {} years old.".format(name, age))
